5a5beac42b8d07b1181bb06be9fecad9655b1aa5
lutchen <lutchen.henrique@gmail.com>
2022-05-26 16:49:01 -0300

    commit estização treinatweet

--- src/data/hooks/pages/useIndex.page.js
+++ src/data/hooks/pages/useIndex.page.js
@@ -1,3 +1,11 @@
 export function useIndex(){
     
+    const user = {
+        name:'Lutchen',
+        username:'Lutchen1',
+        picture: 'https://imagensemoldes.com.br/wp-content/uploads/2020/04/Foto-Homem-Aranha-PNG-1016x1024.png',
+    };
+
+    return {user}
+
 }
